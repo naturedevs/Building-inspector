1a974aa0f749f59f0bd66724e40b675c8c0a0285
Kota <thomaschendev0412@gmail.com>
2024-05-10 14:23:41 +0900

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const API_URL = 'http://localhost:4000'
 export const API_ROUTES = {
-  LOGIN: `${API_URL}/auth/login`,
-  REGISTER: `${API_URL}/auth/register`,
-  GET_USER: `${API_URL}/auth/me`,
+   LOGIN: `${API_URL}/auth/login`,
+   REGISTER: `${API_URL}/auth/register`,
+   GET_USER: `${API_URL}/auth/me`,
 }
