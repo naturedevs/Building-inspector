675343f56b4f707daa691a2e0f4e66e17f3511fa
Kota <thomaschendev0412@gmail.com>
2024-05-14 22:10:45 +0900

    upt constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,6 @@ const API_URL = 'http://localhost:4000'
 export const API_ROUTES = {
    LOGIN: `${API_URL}/auth/login`,
    REGISTER: `${API_URL}/auth/register`,
-   GET_USER: `${API_URL}/auth/me`,
+   ME: `${API_URL}/auth/me`,
+   GET_USER_LIST: `${API_URL}/user/list`
 }
