9356403cf4ae793290fce34665b68edc17dacc79
Kota <thomaschendev0412@gmail.com>
2024-05-22 17:06:55 +0900

    upt

--- src/pages/role/Form.tsx
+++ src/pages/role/Form.tsx
@@ -1,11 +1,8 @@
-import { User } from "./types";
 import { Button, Modal, Card, Col, Collapse, Form, InputGroup, Row } from 'react-bootstrap';
-import Select from 'react-select';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, FieldValues } from "react-hook-form";
 import * as z from "zod";
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { Role } from "../role/types";
 import { API_ROUTES } from '../../utils/constants';
 import toast from "react-hot-toast";
@@ -115,12 +112,12 @@ export function RoleForm (
     return(
     <Modal as="form" centered show={modalShow} onHide={() => setModalShow(false)} keyboard={false} className="modal fade">
         <form onSubmit={handleSubmit((d) => handleRegister(d))}>
-            <Modal.Header closeButton className={`bg-success1`}>
-                <Modal.Title as="h6">{role?.name?role.name:"New Role"}</Modal.Title>
+            <Modal.Header closeButton>
+                <Modal.Title as="h5">{role?"Update":"Add"}</Modal.Title>
             </Modal.Header>
             <Modal.Body>                
                 <div className="mb-3">
-                    <Form.Label htmlFor="form-text1" className=" fs-14 text-dark">Enter title</Form.Label>
+                    <Form.Label htmlFor="form-text1" className=" fs-14 text-dark">Title</Form.Label>
                     <InputGroup>
                         <InputGroup.Text className=""><i className="bx bx-user-check"></i></InputGroup.Text>
                         <Form.Control type="text" {...register('name')} className="" id="form-text1" placeholder=""/>
@@ -131,10 +128,8 @@ export function RoleForm (
                 </div>    
             </Modal.Body>
             <Modal.Footer>
-            <Button variant="secondary" onClick={() => setModalShow(false)}>
-                Cancel
-            </Button>
-            <button className="btn btn-success" type="submit">{role?"Update":"Add"}</button>
+            <Button variant="light" onClick={() => setModalShow(false)}>Cancel</Button>
+            <Button variant="primary" type="submit">{role?"Update":"Add"}</Button>
             </Modal.Footer>
         </form>
     </Modal>
